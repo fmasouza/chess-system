706f6ef4d89b51c6ff361d322d954c0b51be4a38
Fabio Souza <fabmacsouza@gmail.com>
2020-04-17 18:01:04 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -16,11 +15,10 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMath chessMath = new ChessMath();
 		
-		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printBoard(chessMath.getPieces());
+				UI.printMath(chessMath);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
